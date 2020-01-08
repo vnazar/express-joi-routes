65aa33f4894ffe33151d49889d95558ed3a99b99
Vicente <vnazar@anastasia.ai>
2020-01-08 18:51:54 -0300

    feat: Add class ExpressAwesomeRoutes

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 'use strict';
 
 const express = require('express');
+const {createRoutes, ContainerTypes} = require('./dist/express-awesome-routes');
 
+const bodyParser = require('body-parser') 
 const app = express();
 
+class AController {
+  get(_req , res) {
+    res.json({ message: 'A.get' });
+  }
+  post(_req, res) {
+    res.json({ message: 'A.post' });
+  }
+}
+
+const route = [
+  {
+    routerHandler: 'get',
+    route: '/A',
+    controller: AController,
+    function: 'get',
+    // middlewares: [
+    //   (req: Request, _res: Response, next: NextFunction): void => {
+    //     console.log(req.query);
+    //     next();
+    //   },
+    // ],
+  }
+]
+
+console.log(ContainerTypes.Body)
+app.use(bodyParser.json())
+app.use(createRoutes(route))
 app.listen(3000, function() {
   console.log('Listening on port 3000');
 });
--- src/express-awesome-routes.ts
+++ src/express-awesome-routes.ts
@@ -1,14 +1,23 @@
-import { Express, Request, Response, NextFunction, RequestHandler } from 'express';
-import { createValidator, ExpressJoiInstance, ContainerTypes } from 'express-joi-validation';
-import Joi, { Schema } from '@hapi/joi';
+import { Request, Response, NextFunction, RequestHandler, Router } from 'express';
+import { createValidator, ExpressJoiInstance } from 'express-joi-validation';
+import Joi from '@hapi/joi';
+
+export enum ContainerTypes {
+  Body = 'body',
+  Query = 'query',
+  Headers = 'headers',
+  Fields = 'fields',
+  Params = 'params',
+}
 
 type ValidationTypes = ContainerTypes.Body | ContainerTypes.Fields | ContainerTypes.Headers | ContainerTypes.Params | ContainerTypes.Query;
+
 interface ValidatorOpts {
   type: ValidationTypes;
   schema: Joi.Schema;
 }
 
-type Methods =
+type RouterHandlers =
   | 'get'
   | 'post'
   | 'delete'
@@ -42,12 +51,12 @@ type Methods =
  */
 interface BaseRoute {
   route: string;
-  middlewares?: any;
+  middlewares?: RequestHandler[];
 }
 interface Route extends BaseRoute {
-  method: Methods;
+  routerHandler: RouterHandlers;
   controller: any;
-  function: string;
+  method: string;
   validators?: ValidatorOpts[];
 }
 
@@ -61,123 +70,91 @@ function _isDefined<T>(val: T | undefined | null): val is T {
   return typeof (val as T) !== 'undefined' && typeof (val as T) !== null;
 }
 
-function _routerHandler(route: Route): RequestHandler {
-  return (req: Request, res: Response, next: NextFunction): void => {
-    const result: any = new route.controller()[route.function](req, res, next);
-    if (result instanceof Promise) {
-      result.then((result: any) => (_isDefined(result) ? res.send(result) : next()));
-    } else if (_isDefined(result)) {
-      res.json(result);
-    }
-  };
-}
-
-function _createRoutePath(prefix: string, path: string): string {
-  return prefix + path;
-}
-
-function isRoute(val: Route | ProxyRoute): val is Route {
-  return (val as Route).method !== undefined;
-}
-
-function isProxyRoute(val: Route | ProxyRoute): val is ProxyRoute {
-  return (val as ProxyRoute).subRoutes !== undefined;
-}
-
-function _loadRoutes(routes: Routes, app: Express, path?: string, middlewares?: any[]): void {
-  routes.forEach((route: Route | ProxyRoute) => {
-    const parentMiddlewares: any[] = _isDefined(middlewares) ? middlewares : [];
-    const currentMiddlewares: any[] = _isDefined(route.middlewares) ? route.middlewares : [];
-    const allMiddlewares: any[] = parentMiddlewares.concat(currentMiddlewares);
-    const fullPath: string = _isDefined(path) ? _createRoutePath(path, route.route) : route.route;
-
-    if (isRoute(route)) {
-      const validator: ExpressJoiInstance = createValidator();
-      console.log(ContainerTypes.Body);
-      const validatorsOpts: ValidatorOpts[] = _isDefined(route.validators) ? route.validators : [];
-      const validatorHandlers: RequestHandler[] = validatorsOpts.map((validatorOpts: ValidatorOpts) =>
-        validator[validatorOpts.type](validatorOpts.schema),
-      );
-      app[route.method](fullPath, ...allMiddlewares, ...validatorHandlers, _routerHandler(route));
-    } else if (isProxyRoute(route)) {
-      _loadRoutes(route.subRoutes, app, fullPath, allMiddlewares);
-    }
-  });
-}
-export function loadRoutes(routes: Routes, app: Express): void;
-export function loadRoutes(routes: Routes, app: Express, prefix: string): void;
-export function loadRoutes(routes: Routes, app: Express, prefix: string, middlewares: any[]): void;
-export function loadRoutes(routes: Routes, app: Express, prefix?: string, middlewares?: any[]): void {
-  // const validator: ExpressJoiInstance = createValidator({ passError: true });
-  _loadRoutes(routes, app, prefix, middlewares);
-}
-
-//===========================string=============================================
-
-import express from 'express';
-const app: Express = express();
-
-class AController {
-  public get(_req: Request, res: Response): void {
-    res.json({ message: 'A.get' });
+export class ExpressAwesomeRoutes {
+  private readonly _router: Router;
+  public constructor() {
+    this._router = Router();
   }
-  public post(_req: Request, res: Response): void {
-    res.json({ message: 'A.post' });
+
+  private _routerHandler(controller: any, method: string): RequestHandler {
+    return (req: Request, res: Response, next: NextFunction): void => {
+      const result: any = new controller()[method](req, res, next);
+      if (result instanceof Promise) {
+        result.then((result: any) => (_isDefined(result) ? res.send(result) : next()));
+      } else if (_isDefined(result)) {
+        res.json(result);
+      }
+    };
+  }
+
+  private _createRoutePath(prefix: string, path: string): string {
+    return prefix + path;
+  }
+
+  private _isRoute(val: Route | ProxyRoute): val is Route {
+    return (val as Route).routerHandler !== undefined;
+  }
+
+  private _isProxyRoute(val: Route | ProxyRoute): val is ProxyRoute {
+    return (val as ProxyRoute).subRoutes !== undefined;
+  }
+
+  /**
+   * Load routes recursively.
+   *
+   * @private
+   * @param {Routes} routes
+   * @param {Router} router
+   * @param {string} [path]
+   * @param {any[]} [middlewares]
+   * @memberof ExpressAwesomeRoutes
+   */
+  private _loadRoutes(routes: Routes, router: Router, path?: string, middlewares?: any[]): void {
+    routes.forEach((route: Route | ProxyRoute) => {
+      const parentMiddlewares: any[] = _isDefined(middlewares) ? middlewares : [];
+      const currentMiddlewares: any[] = _isDefined(route.middlewares) ? route.middlewares : [];
+      const allMiddlewares: any[] = parentMiddlewares.concat(currentMiddlewares);
+      const fullPath: string = _isDefined(path) ? this._createRoutePath(path, route.route) : route.route;
+
+      if (this._isRoute(route)) {
+        const validator: ExpressJoiInstance = createValidator();
+        const validatorsOpts: ValidatorOpts[] = _isDefined(route.validators) ? route.validators : [];
+        const validatorHandlers: RequestHandler[] = validatorsOpts.map((validatorOpts: ValidatorOpts) => {
+          return validator[validatorOpts.type](validatorOpts.schema);
+        });
+        this._router[route.routerHandler](
+          fullPath,
+          ...allMiddlewares,
+          ...validatorHandlers,
+          this._routerHandler(route.controller, route.routerHandler),
+        );
+      } else if (this._isProxyRoute(route)) {
+        this._loadRoutes(route.subRoutes, router, fullPath, allMiddlewares);
+      }
+    });
+  }
+
+  public create(routes: Routes): Router;
+  public create(routes: Routes, prefix: string): Router;
+  public create(routes: Routes, prefix: string, middlewares: any[]): Router;
+  public create(routes: Routes, prefix?: string, middlewares?: any[]): Router {
+    const router: Router = Router();
+    // const validator: ExpressJoiInstance = createValidator({ passError: true });
+    this._loadRoutes(routes, router, prefix, middlewares);
+    return this._router;
   }
 }
 
-class BController {
-  public get(_req: Request, res: Response): void {
-    res.json({ message: 'B.get' });
-  }
-  public post(_req: Request, res: Response): void {
-    res.json({ message: 'A.post' });
+export function createRoutes(routes: Routes): Router;
+export function createRoutes(routes: Routes, prefix: string): Router;
+export function createRoutes(routes: Routes, prefix: string, middlewares: any[]): Router;
+export function createRoutes(routes: Routes, prefix?: string, middlewares?: any[]): Router {
+  const expressAwesomeRoutes: ExpressAwesomeRoutes = new ExpressAwesomeRoutes();
+  if (middlewares !== undefined && prefix !== undefined) {
+    return expressAwesomeRoutes.create(routes, prefix, middlewares);
+  } else if (prefix !== undefined) {
+    return expressAwesomeRoutes.create(routes, prefix);
+  } else {
+    return expressAwesomeRoutes.create(routes);
   }
 }
-
-const schema: Schema = Joi.object({
-  message: Joi.string().required(),
-  status: Joi.number().required(),
-});
-
-const subRoutesB: Routes = [
-  {
-    method: 'get',
-    route: '/:id',
-    controller: BController,
-    function: 'get',
-    // middlewares: 'middle3()',
-  },
-];
-
-const routes: Routes = [
-  {
-    method: 'get',
-    route: '/A',
-    controller: AController,
-    function: 'get',
-    middlewares: [
-      (req: Request, _res: Response, next: NextFunction): void => {
-        console.log(req.query);
-        next();
-      },
-    ],
-    validators: [{ type: ContainerTypes.Body, schema: schema }],
-  },
-  {
-    method: 'post',
-    route: '/A',
-    controller: AController,
-    function: 'post',
-  },
-  {
-    route: '/B',
-    subRoutes: subRoutesB,
-  },
-];
-
-loadRoutes(routes, app);
-app.listen(3000, () => {
-  console.log('Listening on port: 3000');
-});
-// console.log(app._router.stack);
