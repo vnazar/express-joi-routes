0d6e1854aee39cfe2007d397b535ff88689eb64c
Vicente <vnazar@anastasia.ai>
2020-01-09 18:55:10 -0300

    test: Add test module (empty)

---
+++ tests/express-awesome-routes.test.ts
@@ -0,0 +1,17 @@
+// import { expect } from 'chai';
+// import 'mocha';
+// import { ExpressAwesomeRoutes } from '../src';
+// import { Request, Response, response } from 'express';
+
+// describe('ExpressAwesomeRoutes', () => {
+//   describe('add', () => {
+//     it('', () => {
+//       const ear: ExpressAwesomeRoutes = new ExpressAwesomeRoutes();
+//       const controller: any = function(): void {
+//         function get(req: Request, res: Response): void {
+//           res.json();
+//         }
+//       };
+//     });
+//   });
+// });
