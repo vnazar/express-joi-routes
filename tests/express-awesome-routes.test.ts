e7b6e0587b11dd549e6cfb416af51a826df981e3
Vicente Nazar <vnazar@anastasia.ai>
2020-01-13 11:21:08 -0300

    test: Update base test

--- tests/express-awesome-routes.test.ts
+++ tests/express-awesome-routes.test.ts
@@ -1,17 +1,33 @@
-// import { expect } from 'chai';
-// import 'mocha';
-// import { ExpressAwesomeRoutes } from '../src';
-// import { Request, Response, response } from 'express';
+import { ExpressAwesomeRoutes } from '../src';
+import * as sampleRoutes from './_fixtures/sampleRoutes';
+import request, { Response as SuperTestResponse } from 'supertest';
+import express, { Express, Router, Request, Response } from 'express';
+import bodyParser from 'body-parser';
 
-// describe('ExpressAwesomeRoutes', () => {
-//   describe('add', () => {
-//     it('', () => {
-//       const ear: ExpressAwesomeRoutes = new ExpressAwesomeRoutes();
-//       const controller: any = function(): void {
-//         function get(req: Request, res: Response): void {
-//           res.json();
-//         }
-//       };
-//     });
-//   });
-// });
+describe('ExpressAwesomeRoutes', () => {
+  describe('add', () => {
+    it('Add a simple route (first level route and without validations and middlewares)', () => {
+      const ear: ExpressAwesomeRoutes = new ExpressAwesomeRoutes();
+
+      ear.add(sampleRoutes.basicRoute);
+
+      // const r: Router = ear.getRoutes();
+      // console.log(r.stack[0]);
+      // expect(r.stack[0].route.stack[0].method).toEqual('post');
+      const app: Express = express();
+      app.use(bodyParser.json());
+      const router: Router = Router();
+      router.get('/bar', (req: Request, res: Response) => {
+        res.json({ message: 'ok' });
+      });
+      app.use(router);
+      app.use(ear.getRoutes());
+
+      request(app)
+        .get('/foo')
+        .then((value: SuperTestResponse): void => {
+          expect(value.body).toEqual({ message: 'ok' });
+        });
+    });
+  });
+});
