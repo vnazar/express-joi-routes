1aa334179ca42347ed73809486bc408f3446eb77
Vicente <vnazar@anastasia.ai>
2020-01-21 20:18:12 -0300

    chore: Update config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
+  preset: 'ts-jest',
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+  testMatch: ["**/test/*.(ts|tsx)"],
+
 };
