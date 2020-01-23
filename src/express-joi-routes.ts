f4cbfcba5fd2139611716ed5343665839c86ae0d
Vicente <vnazar@anastasia.ai>
2020-01-23 19:12:04 -0300

    feat: Add bind  to controller

--- src/express-joi-routes.ts
+++ src/express-joi-routes.ts
@@ -154,7 +154,7 @@ export class ExpressJoiRoutes {
         try {
           const validatorsHandlers: RequestHandler[] = this._generateValidatorsHandlers(route.validators);
           const ctrl: any = this._hasConstructor(route.controller) ? new route.controller() : route.controller;
-          this._router[route.method](fullPath, ...mw, ...validatorsHandlers, ctrl[route.function]);
+          this._router[route.method](fullPath, ...mw, ...validatorsHandlers, ctrl[route.function].bind(ctrl));
         } catch (error) {
           throw new Error(error);
         }
